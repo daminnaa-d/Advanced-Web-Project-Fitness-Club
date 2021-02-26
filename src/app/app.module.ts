ff09b4c929d99f5c8594256c6c5dea32da23a1e0
daminnaa-d <dilaratemirtay@gmail.com>
2021-02-26 17:09:52 +0600

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -13,6 +13,15 @@ import { ContactsComponent } from './contacts/contacts.component';
 import { RequestFormComponent } from './request-form/request-form.component';
 import { RequestOutputComponent } from './request-form/request-output/request-output.component';
 import {FormsModule} from '@angular/forms';
+
+const appRoutes: Routes = [
+  {path: '', component: FitnessClubComponent},
+  {path: 'schedule', component: ScheduleComponent},
+  {path: 'services', component: ServicesComponent},
+  {path: 'trainers', component: TrainersComponent},
+  {path: 'contacts', component: ContactsComponent},
+  {path: 'request', component: RequestFormComponent},
+];
 
 @NgModule({
   declarations: [
@@ -30,7 +39,8 @@ import {FormsModule} from '@angular/forms';
     imports: [
         BrowserModule,
         AppRoutingModule,
-        FormsModule
+        FormsModule,
+        RouterModule.forRoot(appRoutes)
     ],
   providers: [],
   bootstrap: [AppComponent]
